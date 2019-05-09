6f3615d2d0642fb6a378195935de653c01ca851b
Matt Thurston <matthews@uneelabs.com>
2019-05-09 19:50:57 -0400

    add missing files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import {A11yModule} from '@angular/cdk/a11y';
+import { A11yModule } from '@angular/cdk/a11y';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, A11yModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, A11yModule],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
